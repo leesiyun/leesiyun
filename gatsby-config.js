17f431cbb163f1bb8b819aaa92ed561e0ba237a9
Siyun Lee <siyun1221@gmail.com>
2022-06-06 15:55:14 +0900

    Update TIL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    siteMetadata: {
-      title: `SIYUN'S PORTFOLIO`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
+  siteMetadata: {
+    title: `SIYUN'S PORTFOLIO`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `posts`,
         path: `${__dirname}/posts/`,
-        }
       },
-      "gatsby-plugin-mdx",
-      "gatsby-transformer-sharp",
-    ],
-}
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
+};
