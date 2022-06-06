c4750fcbe77197f47d389d98130d8bf40c2ec1fd
Siyun Lee <siyun1221@gmail.com>
2022-06-07 06:37:44 +0900

    Update TIL

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,39 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import PostLink from "../components/PostLink";
 
-const ProjectsPage = () => {
-  return <Layout pageTitle="Projects"></Layout>;
+const ProjectsPage = ({
+  data: {
+    allMdx: { nodes },
+  },
+}) => {
+  const Posts = nodes
+    .filter((node) => node.frontmatter.folder === "projects")
+    .map((node) => <PostLink key={node.id} post={node} />);
+  return (
+    <Layout pageTitle="Projects">
+      <ul>{Posts}</ul>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          title
+          slug
+          date
+          folder
+          icon
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
 
 export default ProjectsPage;
